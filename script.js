39830f40c26be7a7ca2984c9bbfd595538e7e803
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-30 16:22:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function firstNonRepeatedChar(str) {
+	for(let i=0;i<str.length-1;i++)
+		{
+			if(str.charAt(i)!=str.charAt(i+1) && str.charAt(i)!=str.charAt(i-1))
+			{
+				return str.charAt(i)
+			}
+		}
  // Write your code here
 }
 const input = prompt("Enter a string");
