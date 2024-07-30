f660ef11f422365e356834319a95582d316582c3
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-30 16:26:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function firstNonRepeatedChar(str) {
 	let s=""
 	for(let i=0;i<str.length-1;i++)
 		{
-			ifstr.charAt(i)!=str.charAt(i+1) && str.charAt(i)!=str.charAt(i-1))
+			if(str.charAt(i)!=str.charAt(i+1) && str.charAt(i)!=str.charAt(i-1))
 			{
 				s=str.charAt(i)
 			}
 		}
 	if(s="")
-		return null
+		return "null"
 	else
 		return s
  // Write your code here
